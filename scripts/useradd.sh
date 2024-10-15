c93d137995c0f76e5af004ca7781a06d2c891a48
snagaram3 <Srihari.ec09@gmail.com>
2024-10-15 13:04:55 -0500

    Update useradd.sh

--- scripts/useradd.sh
+++ scripts/useradd.sh
@@ -1,6 +1,10 @@
-RUN groupadd -r -g 10000 sqlpad && useradd -r -m -u 10000 -g sqlpad sqlpad
+#!/usr/bin/env sh
 
-RUN mkdir -p /var/lib/sqlpad && chown -R sqlpad:sqlpad /var/lib/sqlpad
+groupadd -r -g 10000 sqlpad && \
+    useradd -r -m -u 10000 -g sqlpad sqlpad
 
-RUN sed -i '/^root:x:0:0:root:\/root:\/bin\/bash$/d' /etc/passwd \
+mkdir -p /var/lib/sqlpad && \
+    chown -R sqlpad:sqlpad /var/lib/sqlpad
+
+sed -i '/^root:x:0:0:root:\/root:\/bin\/bash$/d' /etc/passwd \
     && echo "sqlpad:x:10000:10000:sqlpad User:/home/sqlpad:/bin/sh" >> /etc/passwd 
